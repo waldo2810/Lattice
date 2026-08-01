48347d0413fd03dacd6c5fa7d205a3daad7e9bd9
Waldo Sobrino <waldo2810@gmail.com>
2026-08-01 16:33:34 -0500

    Initial Commit

---
+++ Lattice/LatticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatticeApp.swift
+//  Lattice
+//
+//  Created by Waldo Sobrino on 1/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LatticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
